b7f04e798ce24dea443d87e268dab4269df1d4f8
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-10-16 23:13:17 +0300

    Fix formatting of JSON payload in HTTP GET request in basic.py

--- client/basic.py
+++ client/basic.py
@@ -3,8 +3,7 @@ import requests
 # endpoint ="https://httpbin.org/status/200/"
 endpoint ="http://127.0.0.1:8000/api/"
 
-response = requests.get(endpoint,json={"query":
-"hello world"})   # => HTTP [GET] request to the endpoint api
+response = requests.get(endpoint,json={"query":"hello world"})   # => HTTP [GET] request to the endpoint api
 # response = requests.get(endpoint,json={"query":"hello world"})   # => GET request to the endpoint api
 print(response.text)         # => print raw  text response
 
