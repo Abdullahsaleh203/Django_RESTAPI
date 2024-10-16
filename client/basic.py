855b40866f19eb0e50ae3edaf40b2773ee1792a8
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-16 14:22:01 +0300

    Update HTTP request handling in basic.py to use httpbin.org endpoints

--- client/basic.py
+++ client/basic.py
@@ -1,3 +1,13 @@
 import requests
 
-endpoint ="https://www.github.com"
+endpoint ="https://httpbin.org/status/200/"
+endpoint ="https://httpbin.org/anything"
+
+response = requests.get(endpoint,json={"query":
+"hello world"})   # => HTTP [GET] request to the endpoint api
+# response = requests.get(endpoint,json={"query":"hello world"})   # => GET request to the endpoint api
+print(response.text)         # => print raw  text response
+
+# HTTP Response -> HTML
+# REST API  HTTP Request -> JSON
+print(response.json())         # 
