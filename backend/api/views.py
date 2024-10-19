0a60454882337ed2020b64b908f8b7f2a20bee5a
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-10-19 20:08:36 +0300

    Refactor api_home to use model_to_dict for cleaner data retrieval

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,15 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse , HttpRequest
+from django.forms.models import model_to_dict
 from products.models import Product
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data =model_to_dict(model_data)
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
     return JsonResponse(data)
 
