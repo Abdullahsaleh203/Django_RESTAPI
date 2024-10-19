561c6933db0dc0e5dbb58f39892e98d5d58e20f8
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-10-19 22:43:31 +0300

    Refactor api_home to limit fields in model_to_dict and update client to print response headers

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,9 +7,10 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data =model_to_dict(model_data)
+        data =model_to_dict(model_data, fields=['id','title','content'])
         # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data['price'] = model_data.price
+    # return HttpRequest(data, headers={"content-type":"application/json"})
     return JsonResponse(data)
 
--- client/basic.py
+++ client/basic.py
@@ -5,7 +5,8 @@ endpoint ="http://127.0.0.1:8000/api/"
 
 response = requests.get(endpoint,params={"abc":123},json={"query":"hello world"})   # => HTTP [GET] request to the endpoint api
 # response = requests.get(endpoint,json={"query":"hello world"})   # => GET request to the endpoint api
-# print(response.text)         # => print raw  text response
+print(response.text)         # => print raw  text response
+print(response.headers)         # => print raw  text response
 
 
 # HTTP Response -> HTML
