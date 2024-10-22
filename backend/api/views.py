622b435a42328e57775159b191f34931870ffbc5
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-10-22 22:43:27 +0300

    Remove commented-out code in api_home for cleaner implementation

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,6 +11,7 @@ def api_home(request, *args, **kwargs):
         # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data['price'] = model_data.price
+        
     # return HttpRequest(data, headers={"content-type":"application/json"})
     return JsonResponse(data)
 
