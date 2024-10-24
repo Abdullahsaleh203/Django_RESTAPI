67821661ca283886a59ef6eb805716be9f497932
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-24 11:56:47 +0300

    Update api_home to return Response instead of JsonResponse for improved API consistency

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,14 @@ import json
 from django.http import JsonResponse , HttpRequest
 from django.forms.models import model_to_dict
 from products.models import Product
-
+from rest_framework.response import Response
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data =model_to_dict(model_data, fields=['id','title','content'])
-    return JsonResponse(data)
+    return Response(data)
+    # return JsonResponse(data)
     #     jason_data = json.dumps(data) 
     # return HttpRequest(jason_data, headers={"Content-Type":"application/json"})
     # return HttpRequest(data)
