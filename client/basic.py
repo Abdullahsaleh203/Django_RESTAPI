bb34e558737a847b42c3e0b817ba1c6092fcd7de
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-24 11:44:10 +0300

    Clean up print statements in basic.py for improved readability

--- client/basic.py
+++ client/basic.py
@@ -4,12 +4,11 @@ import requests
 endpoint ="http://127.0.0.1:8000/api/"
 
 response = requests.get(endpoint,params={"abc":123},json={"query":"hello world"})   # => HTTP [GET] request to the endpoint api
+# print(response.headers)         # => print raw  text response
 # response = requests.get(endpoint,json={"query":"hello world"})   # => GET request to the endpoint api
-print(response.text)         # => print raw  text response
-print(response.headers)         # => print raw  text response
-
-
+# print(response.text)         # => print raw  text response
+# print(response.headers)         # => print raw  text response
 # HTTP Response -> HTML
 # REST API  HTTP Request -> JSON
-print(response.json())         # 
-print(response.status_code)    # 
+print(response.json())         
+print(response.status_code)     
