6c7d765e2db4ad1c1a60bc7ecc00dfda180a0717
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-24 11:42:54 +0300

    Update api_home to return JsonResponse and remove unused HttpRequest code

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,6 +8,8 @@ def api_home(request, *args, **kwargs):
     data = {}
     if model_data:
         data =model_to_dict(model_data, fields=['id','title','content'])
-    return HttpRequest(data, headers={"content-type":"application/json"})
-    # return JsonResponse(data)
+    return JsonResponse(data)
+    #     jason_data = json.dumps(data) 
+    # return HttpRequest(jason_data, headers={"Content-Type":"application/json"})
+    # return HttpRequest(data)
 
